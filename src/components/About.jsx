21a0d601af51d9d91077896dd3211ff785494d5c
MIRACLE OKEKE <mokeke185@gmail.com>
2023-03-31 10:51:42 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -59,7 +59,7 @@ const About = () => {
               />
             </div>
           </div>
-          <div className="mb-20 max-w-4xl mx-auto">
+          <div className="py-6 max-w-4xl mx-auto">
             <div className=" border-t-2  border-[#D4D4D4] h-px"></div>
 
             <div className="grid md:grid-cols-2 gap-8 ">
